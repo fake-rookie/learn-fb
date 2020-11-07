bfa21005bd38e56d4d7cf4ad877d71eaae1434de
fake-rookie <61480354+fake-rookie@users.noreply.github.com>
2020-11-08 00:19:12 +0800

    more looks like pretty

--- varscreen.c
+++ varscreen.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <linux/fb.h>
 #include <sys/ioctl.h>
 
